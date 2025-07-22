2063b0a6b138740b4dbfb64c527b9a249fb6a174
srinivaschowdary969 <mechden143r@gmail.com>
2025-07-22 11:17:29 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+
+setup(name= 'medical chatbot',
+    version ="0.1.0",
+    auuthor  = "boktair ahmed bappy",
+    author_email="srinivas.bogineni13@gmail.com",
+    packages= find_packages(),
+    install_requires = [])
